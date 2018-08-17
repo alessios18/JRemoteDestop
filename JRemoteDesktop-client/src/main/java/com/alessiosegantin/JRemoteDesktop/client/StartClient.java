e2b4663121b2120eb6e0381f31be5c49974a8996
Alessio Segantin <alessio.segantin@gmail.com>
2018-08-17 11:50:07 +0200

    added the base of the project

---
+++ JRemoteDesktop-client/src/main/java/com/alessiosegantin/JRemoteDesktop/client/StartClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.alessiosegantin.JRemoteDesktop.client;
+
+/**
+ * @author alessio.segantin
+ *
+ */
+public class StartClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JRemoteDesktop-server/src/main/java/com/alessiosegantin/JRemoteDesktop/server/StartServer.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.alessiosegantin.JRemoteDesktop.server;
+
+/**
+ * @author alessio.segantin
+ *
+ */
+public class StartServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
